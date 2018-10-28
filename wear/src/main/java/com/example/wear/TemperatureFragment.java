f964737b4c77289e242719d6708de58f46c8ffb5
Vidit Virmani <30908674+viditvir@users.noreply.github.com>
2018-10-28 17:09:47 -0400

    deleting main_second layout and adding temperature_display layout file

--- wear/src/main/java/com/example/wear/TemperatureFragment.java
+++ wear/src/main/java/com/example/wear/TemperatureFragment.java
@@ -23,7 +23,7 @@ public class TemperatureFragment extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
-        setContentView( R.layout.main_second );
+        setContentView( R.layout.temperature_display );
 
         Thread thread = new Thread(new Runnable() {
 
